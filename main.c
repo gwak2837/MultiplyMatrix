b1720bd7810b0743b77a16084c8d8092af5ad31c
곽태욱 <33145190+rmfpdlxmtidl@users.noreply.github.com>
2018-03-17 13:21:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main()
 {
 	time_t start = 0, end = 0;
 	int **matrix1 = NULL, **matrix2 = NULL, **result = NULL;
-	int length, max;
+	int length, max = 1;
 	int i;
-	int count = 10;
+	int count;
 
 	srand((size_t)time(NULL));
 
@@ -19,8 +19,14 @@ int main()
 	scanf("%d", &length);
 	getchar();
 
+	/*
 	fputs("Input the maximum number of the matrix : ", stdout);
 	scanf("%d", &max);
+	getchar();
+	*/
+
+	fputs("How many times do you want to multiply : ", stdout);
+	scanf("%d", &count);
 	getchar();
 
 	start = clock();
@@ -35,7 +41,7 @@ int main()
 	}
 	end = clock();
 
-	printf("Elasped time : %lf sec\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("\nAverage elasped time of one multiplication : %lf sec\n", ((double)(end - start) / CLOCKS_PER_SEC) / count);
 	system("pause");
 	return 0;
 }
